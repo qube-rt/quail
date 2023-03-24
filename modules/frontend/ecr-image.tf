58a27ad87574a98e8b9c952a55def6007ffdb934
Jakub Musko <umgelurgel@users.noreply.github.com>
2023-03-24 17:24:43 +0100

    Soft-decouple the FE deployment from the rest of the application. (#9)
    * Soft-decouple the FE deployment from the rest of the application.
    
    * Fix tests - update the docker_image trigger to work without the .env file.

--- modules/frontend/ecr-image.tf
+++ modules/frontend/ecr-image.tf
@@ -1,15 +1,61 @@
+locals {
+  frontend_path       = "${path.module}/frontend"
+  frontend_build_path = "${path.module}/frontend/build"
+  s3_origin_id        = "${var.project-name}-s3-origin"
+
+  react_config = <<-EOT
+  REACT_APP_LOGOUT_URL=${var.logout-url}
+
+  REACT_APP_API_HOST=${var.api-root-url}
+  REACT_APP_COGNITO_HOST=https://${var.cognito-domain}.auth.${var.region-primary}.amazoncognito.com
+  REACT_APP_COGNITO_CLIENT_ID=${var.cognito-client-id}
+  EOT
+}
+
+# Generate an .env file to be used by the app
+resource "local_file" "react_env" {
+  filename = "${local.frontend_path}/.env"
+  content  = local.react_config
+}
+
 resource "aws_ecr_repository" "hosting_repository" {
   name                 = local.ecr_image_name
   image_tag_mutability = "MUTABLE"
   tags                 = local.resource_tags
 }
 
+resource "docker_image" "nginx" {
+  name         = local.ecr_image_name
+  keep_locally = true
+
+  build {
+    context    = path.module
+    dockerfile = "nginx/Dockerfile"
+    tag        = [aws_ecr_repository.hosting_repository.repository_url]
+  }
+
+  triggers = {
+    # Only rebuild if any of the source files, assets or dependencies have changed.
+    dir_sha1 = sha1(
+      join("", [for f in concat(
+        tolist(fileset(".", "${path.module}/frontend/src/**")),
+        tolist(fileset(".", "${path.module}/frontend/public/**")),
+        tolist(fileset(".", "${path.module}/frontend/public/.env")),
+        [
+          "${path.module}/frontend/package.json",
+          "${path.module}/frontend/package-lock.json"
+        ]
+      ) : filesha1(f)])
+    )
+  }
+
+  depends_on = [local_file.react_env]
+}
+
 # Build and push the image to the repository
-resource "null_resource" "nginx_docker_build" {
+resource "null_resource" "nginx_image_publish" {
   provisioner "local-exec" {
     command = <<-EOT
-      docker build -f ${path.module}/nginx/Dockerfile -t ${local.ecr_image_name} ${path.module}
-      docker tag ${local.ecr_image_name} ${aws_ecr_repository.hosting_repository.repository_url}
       aws ecr get-login-password --region eu-west-1 | \
         docker login --username AWS --password-stdin ${var.account-primary}.dkr.ecr.${var.region-primary}.amazonaws.com
       docker push ${aws_ecr_repository.hosting_repository.repository_url}
@@ -17,10 +63,9 @@ resource "null_resource" "nginx_docker_build" {
   }
 
   triggers = {
-    run_once = fileexists("${local.frontend_path}/build/asset-manifest.json") ? base64sha256(file("${local.frontend_path}/build/asset-manifest.json")) : "build"
-    # rerun_every_time = uuid()
+    run_once = docker_image.nginx.repo_digest
   }
 
   # Must be run after the frontend resources have been built
-  depends_on = [null_resource.frontend_build]
+  depends_on = [docker_image.nginx]
 }
--- modules/utilities-regional/terraform.tf
+++ modules/utilities-regional/terraform.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = ">=1.3.0"
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 4.0"
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
   }
 }
