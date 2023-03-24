156b40e259b99971d4899385110fe7c7d050bb12
Jakub Musko <umgelurgel@users.noreply.github.com>
2023-03-24 10:21:31 +0100

    Fix login issues. (#8)

--- modules/frontend/frontend/src/AuthCallback.js
+++ modules/frontend/frontend/src/AuthCallback.js
@@ -26,24 +26,26 @@ function AuthCallback() {
     navigate('/auth-initiate');
   }
 
-  axios.post(Config.auth.tokenEndpointUrl, queryString.stringify({
-    grant_type: 'authorization_code',
-    client_id: Config.auth.cognitoClientId,
-    redirect_uri: Config.auth.redirectUrl,
-    code: receivedCode,
-    code_challenge_method: 'S256',
-    code_verifier: codeVerifier,
-  }), {
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-  }).then((response) => {
-    const { id_token: idToken, refresh_token: refreshToken } = response.data;
-    handleLogin(idToken, refreshToken);
-    navigate('/');
-  }).catch(() => {
+  React.useEffect(() => {
+    axios.post(Config.auth.tokenEndpointUrl, queryString.stringify({
+      grant_type: 'authorization_code',
+      client_id: Config.auth.cognitoClientId,
+      redirect_uri: Config.auth.redirectUrl,
+      code: receivedCode,
+      code_challenge_method: 'S256',
+      code_verifier: codeVerifier,
+    }), {
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    }).then((response) => {
+      const { id_token: idToken, refresh_token: refreshToken } = response.data;
+      handleLogin(idToken, refreshToken);
+      navigate('/');
+    }).catch(() => {
     // eslint-disable-next-line no-console
-    console.error('Auth failed, redirecting to logout.');
-    handleLogout();
-  });
+      console.error('Auth failed, redirecting to logout.');
+      handleLogout();
+    });
+  }, []);
 
   return (
     <div className="AuthCallback">
