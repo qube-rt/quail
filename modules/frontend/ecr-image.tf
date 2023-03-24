0ec96ffb8288b1b880446e6172628ee9a3659715
umgelurgel <umgelurgel@gmail.com>
2023-03-24 17:21:25 +0100

    Fix tests - update the docker_image trigger to work without the .env file.

--- modules/frontend/ecr-image.tf
+++ modules/frontend/ecr-image.tf
@@ -40,8 +40,8 @@ resource "docker_image" "nginx" {
       join("", [for f in concat(
         tolist(fileset(".", "${path.module}/frontend/src/**")),
         tolist(fileset(".", "${path.module}/frontend/public/**")),
+        tolist(fileset(".", "${path.module}/frontend/public/.env")),
         [
-          "${path.module}/frontend/.env",
           "${path.module}/frontend/package.json",
           "${path.module}/frontend/package-lock.json"
         ]
