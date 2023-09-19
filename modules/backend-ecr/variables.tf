08bbcddd62247008a03a3b5bfb4385902e851dbc
Crambor <dariuscpt@gmail.com>
2023-09-19 11:21:51 +0100

    chore(backend-ecr): configuration variables for proxy and pip index

--- modules/backend-ecr/variables.tf
+++ modules/backend-ecr/variables.tf
@@ -18,3 +18,27 @@ variable "resource-tags" {
   default     = {}
   description = "Tags to assign to resources provisioned by terraform. Apart from the listed tags, a {part_of: $${project-name}} tag is assigned to all resources."
 }
+
+variable "http_proxy" {
+  type        = string
+  default     = ""
+  description = "Sets http_proxy environment variable during docker build. Useful for airgapped environments that require a proxy."
+}
+
+variable "https_proxy" {
+  type        = string
+  default     = ""
+  description = "Sets https_proxy environment variable during docker build. Useful for airgapped environments that require a proxy."
+}
+
+variable "no_proxy" {
+  type        = string
+  default     = ""
+  description = "Sets no_proxy environment variable during docker build. Useful for airgapped environments that require a proxy."
+}
+
+variable "pip_index_url" {
+  type        = string
+  default     = "https://pypi.python.org/simple"
+  description = "Optional base URL for PyPI that pip uses to fetch python packages during docker build."
+}
